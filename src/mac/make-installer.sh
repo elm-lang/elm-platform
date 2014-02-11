1f393e77d978fd67a226742b0a9ad4ef2f634fb7
Evan Czaplicki <info@elm-lang.org>
2014-02-11 15:32:13 +0100

    Update script to build the compiler from source and copy out generated files automatically

--- src/mac/make-installer.sh
+++ src/mac/make-installer.sh
@@ -2,9 +2,18 @@
 
 set -e
 
-tmpdir=$(mktemp -d -t elm)
+if [ -z "$1" ]; then
+    echo "Missing argument: must specify version of Elm compiler"
+    echo "    ./make-installer.sh 0.11"
+    exit 1
+fi
 
+# Clean-up old pkg files
+rm -f Elm.pkg
+
+# Create directory structure for new pkgs
 installerdir=$(pwd)
+tmpdir=$(mktemp -d -t elm)
 
 contentsdir=$tmpdir
 scriptsdir=$contentsdir/Scripts
@@ -13,11 +22,17 @@ bindir=$contentsdir
 mkdir -p $bindir
 mkdir -p $scriptsdir
 
+# Build Elm compiler and copy required resources to the correct places
+./helper-scripts/build.sh Elm $1
+cp Elm/dist/build/elm/elm $bindir/elm-compiler
+datafilesdir=Elm/data
+
 cp $installerdir/postinstall $scriptsdir
 cp $installerdir/wrapper/elm $bindir
 
-# This is not nice! You should download Elm and build everything from scratch
-for bin in elm-compiler elm-get elm-server elm-repl elm-doc
+# Grab pre-built versions of other executables
+# Probably should build these from source as well
+for bin in elm-get elm-server elm-repl elm-doc
 do
 	whichbin=$(which $bin) || echo ""
 	if [ "$whichbin" = "" ]; then
@@ -32,14 +47,12 @@ do
  	cp $(which $bin) $bindir 
 done
 
-pkgbuild --identifier org.elm-lang.share.pkg --install-location /usr/local/share/elm --root ../share share.pkg
+pkgbuild --identifier org.elm-lang.share.pkg --install-location /usr/local/share/elm --root $datafilesdir share.pkg
 pkgbuild --identifier org.elm-lang.bin.pkg --install-location /usr/local/bin --scripts $scriptsdir --filter 'Scripts.*' --root $tmpdir bin.pkg
 
-productbuild --distribution Distribution.xml --package-path . --resources Resources ElmInstaller.pkg
+productbuild --distribution Distribution.xml --package-path . --resources Resources Elm.pkg
 
 # Clean-up
 rm bin.pkg
 rm share.pkg
-
-# rm -rf $tmpdir
-
+rm -rf $tmpdir
