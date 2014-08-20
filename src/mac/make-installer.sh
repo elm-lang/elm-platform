6d8796af98c8bba21566a40a6d29ad4262fc533e
Evan Czaplicki <info@elm-lang.org>
2014-08-21 00:52:44 +0200

    Overhaul make-installer.hs to use BuildFromSource.hs

--- src/mac/make-installer.sh
+++ src/mac/make-installer.sh
@@ -1,67 +1,97 @@
 #!/bin/sh
+# Run the following command to create an installer:
+#
+#     bash make-installer.sh 0.13
+#
+# This will work iff the version you give is defined in BuildFromSource.hs
+
+
+#### SETUP ####
 
 set -e
 
-# Set version numbers for everything we want to build from source
-version_compiler="0.12.3"
-version_server="0.11.0.1"
-version_repl="0.2.2.1"
-version_get="0.1.2"
-
-# Clean-up old pkg files
-rm -f Elm.pkg
+version=$1
 
 # Create directory structure for new pkgs
-installerdir=$(pwd)
-helpdir=$installerdir/helper-scripts
+pkg_root=$(mktemp -d -t package-artifacts)
+pkg_binaries=$pkg_root
+pkg_scripts=$pkg_root/Scripts
 
-tmpdir=$(mktemp -d -t elm)
-contentsdir=$tmpdir
-scriptsdir=$contentsdir/Scripts
-targetdir=/usr/local/bin
-bindir=$contentsdir
+mkdir -p $pkg_binaries
+mkdir -p $pkg_scripts
 
-mkdir -p $bindir
-mkdir -p $scriptsdir
+usr_binaries=/usr/local/bin
+usr_assets=/usr/local/share/elm
 
-# Build Elm compiler and copy required resources to the correct places
-bash $helpdir/build.sh Elm        $version_compiler
-datafilesdir=projects/Elm/data
 
-cp $installerdir/postinstall $scriptsdir
+#### BUILD ELM PLATFORM ####
 
-# Grab pre-built versions of other executables
-# Probably should build these from source as well
-for bin in elm elm-get elm-server elm-repl elm-doc
-do
-	whichbin=$(which $bin) || echo ""
-	if [ "$whichbin" = "" ]; then
-		echo "File does not exist: $bin"
-		exit 1
-	fi
-	if [ ! -f $whichbin ]; then
-		echo "File does not exist: $bin"
-		exit 1
-	fi
- 	cp $(which $bin) $bindir/$bin-unwrapped
+runhaskell ../BuildFromSource.hs $version
 
-	cat << EOF > $bindir/$bin
+platform=Elm-Platform/$version
+
+#### COPY BINARIES ####
+
+# Create a wrapper around an executable that sets ELM_HOME
+function wrap {
+     cat << EOF > $pkg_binaries/$1
 #!/bin/sh
 
 set -e
 
-export ELM_HOME=/usr/local/share/elm
-$targetdir/$bin-unwrapped \$*
+export ELM_HOME=$usr_assets
+$usr_binaries/$1-unwrapped \$*
 EOF
-	chmod +x $bindir/$bin
+}  
+
+# Copy executables into pkg_binaries directory
+for exe in elm elm-get elm-reactor elm-repl elm-doc
+do
+	cp $platform/bin/$exe $pkg_binaries/$exe-unwrapped
+    wrap $exe
+	chmod +x $pkg_binaries/$exe
 done
 
-pkgbuild --identifier org.elm-lang.share.pkg --install-location /usr/local/share/elm --root $datafilesdir share.pkg
-pkgbuild --identifier org.elm-lang.bin.pkg --install-location /usr/local/bin --scripts $scriptsdir --filter 'Scripts.*' --root $tmpdir bin.pkg
+cp $(pwd)/postinstall $pkg_scripts
 
-productbuild --distribution Distribution.xml --package-path . --resources Resources Elm.pkg
+pkgbuild \
+    --identifier org.elm-lang.binaries.pkg \
+    --install-location $usr_binaries \
+    --scripts $pkg_scripts \
+    --filter 'Scripts.*' \
+    --root $pkg_root \
+    binaries.pkg
 
-# Clean-up
-rm bin.pkg
-rm share.pkg
-rm -rf $tmpdir
+
+#### BUNDLE STATIC ASSETS ####
+
+pkgbuild \
+    --identifier org.elm-lang.compiler-assets.pkg \
+    --install-location $usr_assets/compiler \
+    --root $platform/Elm/data \
+    compiler-assets.pkg
+
+pkgbuild \
+    --identifier org.elm-lang.reactor-assets.pkg \
+    --install-location $usr_assets/reactor \
+    --root $platform/elm-reactor/assets \
+    reactor-assets.pkg
+
+
+#### CREATE PACKAGE ####
+
+rm -f Elm-Platform-$version.pkg
+
+productbuild \
+    --distribution Distribution.xml \
+    --package-path . \
+    --resources Resources \
+    Elm-Platform-$version.pkg
+
+
+#### CLEAN UP ####
+
+rm binaries.pkg
+rm compiler-assets.pkg
+rm reactor-assets.pkg
+rm -rf $pkg_root
