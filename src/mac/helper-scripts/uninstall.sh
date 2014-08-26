9479de377f4ee2bfd473264395fc5c1034c23aab
Evan Czaplicki <info@elm-lang.org>
2014-08-26 18:39:21 +0200

    remove elm-reactor instead of elm-server

--- src/mac/helper-scripts/uninstall.sh
+++ src/mac/helper-scripts/uninstall.sh
@@ -6,7 +6,7 @@ echo "Warning: You are about to remove all Elm executables!"
 
 installdir=/usr/local/bin
 
-for bin in elm elm-compiler elm-get elm-server elm-repl elm-doc
+for bin in elm elm-compiler elm-get elm-reactor elm-repl elm-doc
 do
 	if [ -f $installdir/$bin ]; then
 		sudo rm -f $installdir/$bin
