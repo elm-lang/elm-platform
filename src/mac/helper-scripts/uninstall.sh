20a1aab5f1ed53bd76ed2001248822b761eaa647
Evan Czaplicki <info@elm-lang.org>
2014-09-10 12:52:15 -0700

    Make the uninstall script more comprehensive

--- src/mac/helper-scripts/uninstall.sh
+++ src/mac/helper-scripts/uninstall.sh
@@ -6,11 +6,15 @@ echo "Warning: You are about to remove all Elm executables!"
 
 installdir=/usr/local/bin
 
-for bin in elm elm-compiler elm-get elm-reactor elm-repl elm-doc
+for bin in elm elm-compiler elm-get elm-reactor elm-repl elm-doc elm-server
 do
 	if [ -f $installdir/$bin ]; then
 		sudo rm -f $installdir/$bin
 	fi
+	if [ -f $installdir/$bin-unwrapped ]; then
+		sudo rm -f $installdir/$bin-unwrapped
+	fi
+
 done
 
 sharedir=/usr/local/share/elm
