8dfecb9bb0c0b1730cc5139d6355aaf13eee9789
Pascal Hartig <passy@twitter.com>
2014-11-26 22:47:31 +0000

    Check out shallow git copies
    Since the repositories are throw-away copies, it's not necessary to pull
    the entire history.

--- src/BuildFromSource.hs
+++ src/BuildFromSource.hs
@@ -102,7 +102,7 @@ makeRepos artifactDirectory repos =
 makeRepo :: FilePath -> String -> String -> IO ()
 makeRepo root projectName version =
  do  -- get the right version of the repo
-    git [ "clone", "https://github.com/elm-lang/" ++ projectName ++ ".git" ]
+    git [ "clone", "--depth=1", "https://github.com/elm-lang/" ++ projectName ++ ".git" ]
     setCurrentDirectory projectName
     git [ "checkout", version ]
     git [ "pull" ]
