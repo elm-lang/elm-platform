1c2bcc9321577d429a76defecfe5a420c49e2340
Evan Czaplicki <info@elm-lang.org>
2014-11-26 21:22:58 -0800

    Add latest binaries to uninstall script

--- src/mac/helper-scripts/uninstall.sh
+++ src/mac/helper-scripts/uninstall.sh
@@ -6,7 +6,7 @@ echo "Warning: You are about to remove all Elm executables!"
 
 installdir=/usr/local/bin
 
-for bin in elm elm-compiler elm-get elm-reactor elm-repl elm-doc elm-server
+for bin in elm elm-compiler elm-get elm-reactor elm-repl elm-doc elm-server elm-package elm-make
 do
 	if [ -f $installdir/$bin ]; then
 		sudo rm -f $installdir/$bin
