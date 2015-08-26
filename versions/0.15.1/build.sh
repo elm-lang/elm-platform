16d30b6463078fd71a2be84c6e55596dee65c341
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2015-08-26 12:34:19 +0200

    Trying to fix issues with build script

--- versions/0.15.1/build.sh
+++ versions/0.15.1/build.sh
@@ -9,7 +9,8 @@ cabal sandbox init
 git clone https://github.com/elm-lang/elm-compiler.git
 cd elm-compiler
 git checkout tags/0.15.1 --quiet
-cabal sandbox init --sandbox ..
+cabal sandbox init --sandbox ../.cabal-sandbox
+cp ../cabal.config .
 cabal install -j
 cd ..
 
@@ -17,7 +18,8 @@ cd ..
 git clone https://github.com/elm-lang/elm-package.git
 cd elm-package
 git checkout tags/0.5.1 --quiet
-cabal sandbox init --sandbox ..
+cabal sandbox init --sandbox ../.cabal-sandbox
+cp ../cabal.config .
 cabal install -j
 cd ..
 
@@ -25,7 +27,8 @@ cd ..
 git clone https://github.com/elm-lang/elm-make.git
 cd elm-make
 git checkout tags/0.2 --quiet
-cabal sandbox init --sandbox ..
+cabal sandbox init --sandbox ../.cabal-sandbox
+cp ../cabal.config .
 cabal install -j
 cd ..
 
@@ -33,7 +36,8 @@ cd ..
 git clone https://github.com/elm-lang/elm-reactor.git
 cd elm-reactor
 git checkout tags/0.3.2 --quiet
-cabal sandbox init --sandbox ..
+cabal sandbox init --sandbox ../.cabal-sandbox
+cp ../cabal.config .
 cabal install -j
 cd ..
 
@@ -41,6 +45,7 @@ cd ..
 git clone https://github.com/elm-lang/elm-repl.git
 cd elm-repl
 git checkout tags/0.4.2 --quiet
-cabal sandbox init --sandbox ..
+cabal sandbox init --sandbox ../.cabal-sandbox
+cp ../cabal.config .
 cabal install -j
 cd ..
