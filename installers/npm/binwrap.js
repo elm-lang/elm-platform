317ffd388b183c7abcfd734aba03c26fcca486a8
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-25 16:53:48 -0700

    Use path.join in binwrappers

--- installers/npm/binwrap.js
+++ installers/npm/binwrap.js
@@ -1,6 +1,9 @@
+var path = require("path");
+var getPathTo = require(path.join(__dirname, "index")).getPathTo
+var spawn = require("child_process").spawn;
+
 module.exports = function(executable) {
-  var filename = require("./").getPathTo(executable);
-  var spawn = require("child_process").spawn;
+  var filename = getPathTo(executable);
   var input = process.argv.slice(2);
 
   spawn(filename, input, {stdio: 'inherit'})
