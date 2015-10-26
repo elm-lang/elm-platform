e069d2f1807d887ca617a61f86bb50c00a118fd5
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-26 13:50:14 -0700

    Make sure to exit with an erroneous exit code when building from source fails.

--- installers/npm/buildFromSource.js
+++ installers/npm/buildFromSource.js
@@ -5,5 +5,5 @@ platform.buildFromSource().then(function() {
   console.log("Successfully built from source.");
 }, function(exitCode) {
   console.error("Error - building from source failed to complete.");
-  process.exit(exitCode);
+  process.exit(exitCode || 1);
 });
