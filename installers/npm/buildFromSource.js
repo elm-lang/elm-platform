72db924ba125a393f2354af313476591f49eea89
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-26 13:52:31 -0700

    Make sure to exit with a nonzero exit code.

--- installers/npm/buildFromSource.js
+++ installers/npm/buildFromSource.js
@@ -4,6 +4,12 @@ var platform = require(path.join(__dirname, "platform"));
 platform.buildFromSource().then(function() {
   console.log("Successfully built from source.");
 }, function(exitCode) {
-  console.error("Error - building from source failed to complete.");
-  process.exit(exitCode || 1);
+  if (typeof exitCode === "string") {
+    console.error(exitCode);
+    process.exit(1);
+  } else {
+    console.error("Error - building from source failed to complete.");
+
+    process.exit(exitCode === 0 ? 1 : exitCode);
+  }
 });
