f618d8ef8b3aee07317e2fa807d13471aca48757
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-26 14:39:25 -0700

    Don't try to set child process env; rather, amend current PATH.

--- installers/npm/platform.js
+++ installers/npm/platform.js
@@ -3,25 +3,21 @@ var path = require("path");
 var packageInfo = require(path.join(__dirname, "package.json"));
 var spawn = require("child_process").spawn;
 
-require("es6-object-assign").polyfill();
-
 // Use major.minor.patch from version string - e.g. "1.2.3" from "1.2.3-alpha"
 var elmVersion = packageInfo.version.replace(/^(\d+\.\d+\.\d+).*$/, "$1");
 var distDir = path.join(__dirname, "Elm-Platform", elmVersion, ".cabal-sandbox", "bin");
 
 function buildFromSource() {
   return new Promise(function(resolve, reject) {
-    var childEnv = Object.assign({}, process.env);
-
     // From the "build from source" docs:
     //
     // Now that you have chosen a home for Elm-Platform/, add the absolute path
     // to Elm-Platform/0.15.1/.cabal-sandbox/bin to your PATH. This is
     // necessary to successfully build elm-reactor which relies on elm-make.
-    childEnv.PATH = (childEnv.PATH || "").split(":").concat([distDir]).join(":");
+    process.env.PATH = (process.env.PATH || "").split(":").concat([distDir]).join(":");
 
     var child = spawn("runhaskell", ["BuildFromSource.hs", elmVersion],
-      {stdio: "inherit", env: childEnv});
+      {stdio: "inherit"});
 
     child.on("exit", function(exitCode) {
       if (exitCode === 0) {
