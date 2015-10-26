a8fdd1a4451b4cbbd4b63d9777cffcc09ae1f255
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-26 13:53:09 -0700

    Polyfill Object.assign

--- installers/npm/platform.js
+++ installers/npm/platform.js
@@ -3,6 +3,8 @@ var path = require("path");
 var packageInfo = require(path.join(__dirname, "package.json"));
 var spawn = require("child_process").spawn;
 
+require("es6-object-assign").polyfill();
+
 // Use major.minor.patch from version string - e.g. "1.2.3" from "1.2.3-alpha"
 var elmVersion = packageInfo.version.replace(/^(\d+\.\d+\.\d+).*$/, "$1");
 var distDir = path.join(__dirname, "Elm-Platform", elmVersion, ".cabal-sandbox", "bin");
