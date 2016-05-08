b69d0206b10ebc8c6cbc8b4fbc7033b4cd3e5672
Evan Czaplicki <info@elm-lang.org>
2016-05-08 01:54:06 -0700

    Add section for 0.17 in BuildFromSource.hs

--- installers/BuildFromSource.hs
+++ installers/BuildFromSource.hs
@@ -59,6 +59,14 @@ configs =
         , "elm-repl"     =: "master"
         ]
     ,
+      "0.17" =:
+        [ "elm-compiler" =: "0.17"
+        , "elm-package"  =: "0.17"
+        , "elm-make"     =: "0.17"
+        , "elm-reactor"  =: "0.17"
+        , "elm-repl"     =: "0.17"
+        ]
+    ,
       "0.16" =:
         [ "elm-compiler" =: "0.16"
         , "elm-package"  =: "0.16"
