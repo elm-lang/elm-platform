f4a8326e2bf66c90e124a968e6ef6b104d587c40
Evan Czaplicki <evancz@users.noreply.github.com>
2016-08-30 12:49:55 -0700

    Make the build script nicer

--- installers/BuildFromSource.hs
+++ installers/BuildFromSource.hs
@@ -34,34 +34,36 @@ and testing interactions between projects.
 -}
 module Main where
 
-import qualified Data.List          as List
-import qualified Data.Map           as Map
-import           System.Directory   (createDirectoryIfMissing,
-                                     getCurrentDirectory, setCurrentDirectory)
-import           System.Environment (getArgs)
-import           System.Exit        (ExitCode, exitFailure)
-import           System.FilePath    ((</>))
-import           System.IO          (hPutStrLn, stderr, stdout)
-import           System.Process     (rawSystem, readProcess)
-import           System.Info        (compilerVersion)
-import           Data.Version       (Version, makeVersion, parseVersion, showVersion)
-import           System.Directory   (findExecutable)
-import           Text.ParserCombinators.ReadP (readP_to_S)
-import           Control.Monad      ()
-import           Text.Printf        (hPrintf)
+import qualified Data.List as List
+import qualified Data.Map as Map
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory, setCurrentDirectory)
+import System.Environment (getArgs)
+import System.Exit (ExitCode, exitFailure)
+import System.FilePath ((</>))
+import System.IO (hPutStrLn, stderr, stdout)
+import System.Process (rawSystem, readProcess)
+import System.Info (compilerVersion)
+import qualified Data.Version as Vsn
+import System.Directory  (findExecutable)
+import Text.ParserCombinators.ReadP (readP_to_S)
 
-minCabalVersion = makeVersion [ 1, 18 ]
-
-data GHCVersion = AnyGHC | GHC Version
 
 (=:) = (,)
-withAtLeast = (,)
 
-configs :: Map.Map String (GHCVersion, [(String, String)])
+
+data Config =
+  Config
+    { elmVersion :: String
+    , ghcVersion :: [Int]
+    , dependencies :: [(String, String)]
+    }
+
+
+configs :: Map.Map String Config
 configs =
-  Map.fromList
+  Map.fromList $ map (\config -> (elmVersion config, config)) $
     [
-      "master" =: withAtLeast (GHC $ makeVersion [7, 10])
+      Config "master" [7,10] $
         [ "elm-compiler" =: "master"
         , "elm-package"  =: "master"
         , "elm-make"     =: "master"
@@ -69,7 +71,7 @@ configs =
         , "elm-repl"     =: "master"
         ]
     ,
-      "0.17.1" =: withAtLeast (GHC $ makeVersion [7, 10])
+      Config "0.17.1" [7,10] $
         [ "elm-compiler" =: "0.17.1"
         , "elm-package"  =: "0.17.1"
         , "elm-make"     =: "0.17.1"
@@ -77,7 +79,7 @@ configs =
         , "elm-repl"     =: "0.17.1"
         ]
     ,
-      "0.17" =: withAtLeast (GHC $ makeVersion [7, 10])
+      Config "0.17" [7,10] $
         [ "elm-compiler" =: "0.17"
         , "elm-package"  =: "0.17"
         , "elm-make"     =: "0.17"
@@ -85,7 +87,7 @@ configs =
         , "elm-repl"     =: "0.17"
         ]
     ,
-      "0.16" =: withAtLeast (GHC $ makeVersion [7, 10])
+      Config "0.16" [7,10] $
         [ "elm-compiler" =: "0.16"
         , "elm-package"  =: "0.16"
         , "elm-make"     =: "0.16"
@@ -93,7 +95,7 @@ configs =
         , "elm-repl"     =: "0.16"
         ]
     ,
-      "0.15.1" =: withAtLeast (GHC $ makeVersion [7, 8])
+      Config "0.15.1" [7,8] $
         [ "elm-compiler" =: "0.15.1"
         , "elm-package"  =: "0.5.1"
         , "elm-make"     =: "0.2"
@@ -101,7 +103,7 @@ configs =
         , "elm-repl"     =: "0.4.2"
         ]
     ,
-      "0.15" =: withAtLeast (GHC $ makeVersion [7, 8])
+      Config "0.15" [7,8] $
         [ "elm-compiler" =: "0.15"
         , "elm-package"  =: "0.5"
         , "elm-make"     =: "0.1.2"
@@ -109,7 +111,7 @@ configs =
         , "elm-repl"     =: "0.4.1"
         ]
     ,
-      "0.14.1" =: withAtLeast AnyGHC
+      Config "0.14.1" [7,6] $
         [ "elm-compiler" =: "0.14.1"
         , "elm-package"  =: "0.4"
         , "elm-make"     =: "0.1.1"
@@ -117,7 +119,7 @@ configs =
         , "elm-repl"     =: "0.4"
         ]
     ,
-      "0.14" =: withAtLeast AnyGHC
+      Config "0.14" [7,6] $
         [ "elm-compiler" =: "0.14"
         , "elm-package"  =: "0.2"
         , "elm-make"     =: "0.1"
@@ -125,14 +127,14 @@ configs =
         , "elm-repl"     =: "0.4"
         ]
     ,
-      "0.13" =: withAtLeast AnyGHC
+      Config "0.13" [7,6] $
         [ "Elm"         =: "0.13"
         , "elm-reactor" =: "0.1"
         , "elm-repl"    =: "0.3"
         , "elm-get"     =: "0.1.3"
         ]
     ,
-      "0.12.3" =: withAtLeast AnyGHC
+      Config "0.12.3" [7,6] $
         [ "Elm"        =: "0.12.3"
         , "elm-server" =: "0.11.0.1"
         , "elm-repl"   =: "0.2.2.1"
@@ -140,94 +142,119 @@ configs =
         ]
     ]
 
-checkGHCVersion AnyGHC = return ()
-checkGHCVersion (GHC minGHCVersion) =
-  if compilerVersion < minGHCVersion then
-    do
-      hPrintf stderr "You need at least GHC version %s to build this version of Elm.\n" (showVersion minGHCVersion)
-      exitFailure
-  else
-    hPrintf stdout "Using GHC version %s.\n" (showVersion compilerVersion)
-
-checkCabalVersion =
-  do
-    path <- findExecutable "cabal"
-    case path of
-      Nothing -> do hPrintf stderr "It looks like cabal is not installed. You need at least version %s.\n" (showVersion minCabalVersion)
-                    exitFailure
-      Just path -> do
-        versionString <- readProcess path [ "--numeric-version" ] ""
-        let version = fst . last . readP_to_S parseVersion $ versionString
-        if version < minCabalVersion then
-          do
-            hPrintf stderr "You need at least cabal version %s to build Elm.\n" (showVersion minCabalVersion)
-            exitFailure
-        else
-          hPrintf stdout "Using cabal version %s.\n" (showVersion version)
 
 main :: IO ()
 main =
- do args <- getArgs
-    case args of
-      [version] | Map.member version configs ->
-        do let (minGHCVersion, repos) = configs Map.! version
-           checkGHCVersion minGHCVersion
-           checkCabalVersion
-           let artifactDirectory = "Elm-Platform" </> version
-           makeRepos artifactDirectory version repos
+  do  args <- getArgs
+      case args of
+        [version] ->
+          case Map.lookup version configs of
+            Nothing ->
+              failure $
+                "You want me to build `" ++ version ++ "` but the only options are:\n    "
+                ++ List.intercalate ", " (Map.keys configs)
 
-      _ ->
-        do hPutStrLn stderr $
-               "Expecting one of the following values as an argument:\n" ++
-               "    " ++ List.intercalate ", " (Map.keys configs)
-           exitFailure
+            Just (Config _ expectedGhcVersion dependencies) ->
+              do  checkGhc expectedGhcVersion
+                  checkCabal
+                  makeRepos ("Elm-Platform" </> version) version dependencies
+
+        _ ->
+          failure $
+            "Expecting one of the following values as the only argument:\n    "
+            ++ List.intercalate ", " (Map.keys configs)
+
+
+failure :: String -> IO ()
+failure msg =
+  do  hPutStrLn stderr msg
+      exitFailure
 
 
 makeRepos :: FilePath -> String -> [(String, String)] -> IO ()
 makeRepos artifactDirectory version repos =
- do createDirectoryIfMissing True artifactDirectory
-    setCurrentDirectory artifactDirectory
-    writeFile "cabal.config" "split-objs: True"
-    root <- getCurrentDirectory
-    mapM_ (uncurry (makeRepo root)) repos
+  do  createDirectoryIfMissing True artifactDirectory
+      setCurrentDirectory artifactDirectory
+      writeFile "cabal.config" "split-objs: True"
+      root <- getCurrentDirectory
+      mapM_ (uncurry (makeRepo root)) repos
 
-    cabal [ "update" ]
+      cabal [ "update" ]
 
-    -- create a sandbox for installation
-    cabal [ "sandbox", "init" ]
+      -- create a sandbox for installation
+      cabal [ "sandbox", "init" ]
 
-    -- add each of the sub-directories as a sandbox source
-    cabal ([ "sandbox", "add-source" ] ++ map fst repos)
+      -- add each of the sub-directories as a sandbox source
+      cabal ([ "sandbox", "add-source" ] ++ map fst repos)
 
-    -- install all of the packages together in order to resolve transitive dependencies robustly
-    -- (install the dependencies a bit more quietly than the elm packages)
-    cabal ([ "install", "-j", "--only-dependencies", "--ghc-options=\"-w\"" ]
-           ++ (if version <= "0.15.1" then [ "--constraint=fsnotify<0.2" ] else [])
-           ++ map fst repos)
-    cabal ([ "install", "-j" ]
-           ++ (if version <= "0.15.1" then [ "--ghc-options=\"-XFlexibleContexts\"" ] else [])
-           ++ filter (/= "elm-reactor") (map fst repos))
+      -- install all of the packages together in order to resolve transitive dependencies robustly
+      -- (install the dependencies a bit more quietly than the elm packages)
+      cabal ([ "install", "-j", "--only-dependencies", "--ghc-options=\"-w\"" ]
+             ++ (if version <= "0.15.1" then [ "--constraint=fsnotify<0.2" ] else [])
+             ++ map fst repos)
+      cabal ([ "install", "-j" ]
+             ++ (if version <= "0.15.1" then [ "--ghc-options=\"-XFlexibleContexts\"" ] else [])
+             ++ filter (/= "elm-reactor") (map fst repos))
 
-    -- elm-reactor needs to be installed last because of a post-build dependency on elm-make
-    cabal [ "install", "-j", "elm-reactor" ]
+      -- elm-reactor needs to be installed last because of a post-build dependency on elm-make
+      cabal [ "install", "-j", "elm-reactor" ]
 
-    return ()
+      return ()
+
 
 makeRepo :: FilePath -> String -> String -> IO ()
 makeRepo root projectName version =
   do  -- get the right version of the repo
-    git [ "clone", "https://github.com/elm-lang/" ++ projectName ++ ".git" ]
-    setCurrentDirectory projectName
-    git [ "checkout", version, "--quiet" ]
+      git [ "clone", "https://github.com/elm-lang/" ++ projectName ++ ".git" ]
+      setCurrentDirectory projectName
+      git [ "checkout", version, "--quiet" ]
 
-    -- move back into the root
-    setCurrentDirectory root
+      -- move back into the root
+      setCurrentDirectory root
+
+
+
+-- CHECK HASKELL VERSIONS
+
+
+checkGhc :: [Int] -> IO ()
+checkGhc expected =
+  if take 2 expected == take 2 (Vsn.versionBranch compilerVersion) then
+    putStrLn $ "Using GHC " ++ Vsn.showVersion compilerVersion
+
+  else
+    failure $
+      "You need GHC " ++ List.intercalate "." (map show expected)
+      ++ " to build this version of Elm.\nYou are using GHC "
+      ++ Vsn.showVersion compilerVersion ++ " instead."
+
+
+checkCabal :: IO ()
+checkCabal =
+  do  path <- findExecutable "cabal"
+      case path of
+        Nothing ->
+          failure $ "It looks like cabal is not installed. You need at least version 1.18 or higher."
+
+        Just path ->
+          do  versionString <- readProcess path [ "--numeric-version" ] ""
+              let version = fst $ last $ readP_to_S Vsn.parseVersion versionString
+              if [1,18] <= Vsn.versionBranch version
+                then
+                  putStrLn $ "Using Cabal " ++ versionString
+                else
+                  failure $ "You need cabal 1.18 or higher to build Elm. You are using cabal " ++ versionString
+
 
 
 -- HELPER FUNCTIONS
 
+
 cabal :: [String] -> IO ExitCode
-cabal = rawSystem "cabal"
+cabal =
+  rawSystem "cabal"
+
 
 git :: [String] -> IO ExitCode
-git = rawSystem "git"
+git =
+  rawSystem "git"
