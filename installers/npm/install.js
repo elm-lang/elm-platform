c83832cd38091033288a62d8b8ce9f1694454d9a
Richard Feldman <richard.t.feldman@gmail.com>
2017-08-19 16:56:07 +0000

    Simplify npm installer (#194)
    * :skull: dead code
    
    * Make Appveyor and Travis test global install too.
    
    * Remove obsolete binwrapper system.
    
    * Update to node 4.0.0, upgrade tar and request, remove promise and mkdirp
    
    * :art: comment
    
    * Clean up .gitignore
    
    * :skull: dead code
    
    * Use --no-spin on npm CI builds
    
    * Use binstall
    
    * Fix Travis
    
    * Make Appveyor's build approach consistent with Travis's
    
    * Upgrade to binstall 1.1.0
    
    * Upgrade to binstall 1.2.0

--- installers/npm/install.js
+++ installers/npm/install.js
@@ -1,113 +1,34 @@
-var Promise = require("promise");
+var binstall = require("binstall");
 var path = require("path");
-var platform = require(path.join(__dirname, "platform"));
 var fs = require("fs");
-var request = require("request");
-var tar = require("tar");
-var zlib = require("zlib");
-var mkdirp = require("mkdirp");
-var distDir = platform.distDir;
-var shareReactorDir = platform.shareReactorDir;
+var packageInfo = require(path.join(__dirname, "package.json"));
 
-function checkBinariesPresent() {
-  return Promise.all(
-    platform.executables.map(function(executable) {
-      var executablePath = platform.executablePaths[executable];
+// Use major.minor.patch from version string - e.g. "1.2.3" from "1.2.3-alpha"
+var binVersion = packageInfo.version.replace(/^(\d+\.\d+\.\d+).*$/, "$1");
 
-      return new Promise(function(resolve, reject) {
-        fs.stat(executablePath, function(err, stats) {
-          if (err) {
-            reject(executable + " was not found.");
-          } else if (!stats.isFile()) {
-            reject(executable + " was not a file.");
-          } else {
-            resolve();
-          }
-        });
-      });
-    })
-  );
-}
+// 'arm', 'ia32', or 'x64'.
+var arch = process.arch;
 
-function downloadBinaries() {
-  return new Promise(function(resolve, reject) {
-    // 'arm', 'ia32', or 'x64'.
-    var arch = process.arch;
+// 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+var operatingSystem = process.platform;
 
-    //'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
-    var operatingSystem = process.platform;
+var filename = operatingSystem + "-" + arch + ".tar.gz";
+var url = "https://dl.bintray.com/elmlang/elm-platform/"
+  + binVersion + "/" + filename;
 
-    var filename = operatingSystem + "-" + arch + ".tar.gz";
-    var url = "https://dl.bintray.com/elmlang/elm-platform/"
-      + platform.elmVersion + "/" + filename;
-
-    var untar = tar.Extract({path: distDir, strip: 1})
-        .on("error", function(error) {
-          reject("Error extracting " + filename + " - " + error);
-        })
-        .on("end", function() {
-          if (!fs.existsSync(distDir)) {
-            reject(
-                "Error extracting executables: extraction finished, but",
-                distDir, "directory was not created.\n" +
-                "Current directory contents: " + fs.readdirSync(__dirname)
-            );
-          }
-
-          if (!fs.statSync(distDir).isDirectory()) {
-            reject(
-                "Error extracting executables: extraction finished, but" +
-                distDir + "ended up being a file, not a directory. " +
-                "This can happen when the .tar.gz file contained the " +
-                "binaries directly, instead of containing a directory with " +
-                "the files inside.");
-          }
-
-          checkBinariesPresent().then(function() {
-                resolve("Successfully downloaded and processed " + filename);
-              }, function(error) {
-                console.error("Error extracting executables...");
-                console.error("Expected these executables to be in", distDir, " - ", platform.executables);
-                console.error("...but got these contents instead:", fs.readdirSync(distDir));
-
-                reject(error);
-              }
-          );
-        });
-
-    var gunzip = zlib.createGunzip()
-        .on("error", function(error) {
-          reject("Error decompressing " + filename + " " + error);
-        });
-
-    request.get(url, function(error, response) {
-      if(error) {
-        reject("Error communicating with URL " + url + " " + error);
-        return;
-      }
-      if (response.statusCode == 404) {
-        reject("Unfortunately, there are currently no Elm Platform binaries available for your operating system and architecture.\n\nIf you would like to build Elm from source, there are instructions at https://github.com/elm-lang/elm-platform#build-from-source\n");
-        return;
-      }
-
-      if (!fs.existsSync(distDir)) {
-        mkdirp.sync(distDir);
-      }
-
-      console.log("Downloading Elm binaries from " + url);
-
-      response.on("error", function(error) {
-        reject("Error receiving " + url);
-      });
-    }).pipe(gunzip).pipe(untar);
-  });
-}
-
-downloadBinaries().then(function(successMessages) {
-  successMessages.forEach(function(message) {
-    console.log(message);
-  })
-}, function(errorMsg) {
-  console.error(errorMsg);
-  process.exit(1);
+var binariesDir = path.join(__dirname, "binaries");
+var packageInfo = require(path.join(__dirname, "package.json"));
+var binaryExtension = process.platform === "win32" ? ".exe" : "";
+var executablePaths = Object.keys(packageInfo.bin).map(function(executable) {
+  return path.join(binariesDir, executable + binaryExtension);
 });
+var errorMessage = "Unfortunately, there are no Elm Platform " + binVersion + " binaries available for your operating system and architecture.\n\nIf you would like to build Elm from source, there are instructions at https://github.com/elm-lang/elm-platform#build-from-source\n";
+
+binstall(url, {path: binariesDir, strip: 1},
+  {verbose: true, verify: executablePaths, errorMessage: errorMessage}
+).then(function(successMessage) {
+    console.log(successMessage);
+  }, function(errorMessage) {
+    console.error(errorMessage);
+    process.exit(1);
+  });
