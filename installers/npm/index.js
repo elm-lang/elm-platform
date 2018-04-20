458b2f368ad69a766e365d77aef15fe8918c579c
Richard Feldman <richard.t.feldman@gmail.com>
2018-04-20 14:19:16 -0400

    use GitHub Releases over Bintray, add `--ignore-scripts` compatibility (#233)
    * move binaries/ to bin/
    
    * Require node>=5.2.0 for npx
    
    * Use binwrap over binstall
    
    * Make default binaries friendly to --ignore-scripts
    
    * Specify main in package.json
    
    * fixup! Make default binaries friendly to --ignore-scripts
    
    * Bump version to 0.18.0-exp
    
    * chmod +x bin/*
    
    * Fix packageInfo
    
    * Add bin/ contents to files
    
    * Bump to exp3
    
    * Add bin/ to files
    
    * Bump to exp4 for release

---
+++ installers/npm/index.js
@@ -0,0 +1,20 @@
+var binwrap = require("binwrap");
+var path = require("path");
+
+var packageInfo = require(path.join(__dirname, "package.json"));
+// Use major.minor.patch from version string - e.g. "1.2.3" from "1.2.3-alpha"
+var binVersion = packageInfo.version.replace(/^(\d+\.\d+\.\d+).*$/, "$1");
+
+var root = "https://github.com/elm-lang/elm-platform/releases/download/" + binVersion + "-exp/elm-platform";
+
+module.exports = binwrap({
+  binaries: ["elm", "elm-make", "elm-repl", "elm-reactor", "elm-package"],
+  urls: {
+    "darwin-x64": root + "-macos.tar.gz",
+    "win32-x64": root + "-windows.tar.gz",
+    "win32-ia32": root + "-windows.tar.gz",
+    "linux-x64": root + "-linux-32bit.tar.gz",
+    "linux-ia32": root + "-linux-64bit.tar.gz"
+  }
+});
+
